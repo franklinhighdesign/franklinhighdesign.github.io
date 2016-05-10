33c7530accb339ded35dfe396a26d6f7bcdc0ad2
RJamieson <rjamie2070@gmail.com>
2016-05-10 12:24:23 -0700

    Form Update

---
+++ rjamieson/FormPractice/scripts.js
@@ -0,0 +1,22 @@
+var form = document.forms.practiceForm;
+
+form.addEventListener("submit", displayPerson, false); 
+
+function displayPerson (event) {
+	event.preventDefault();
+
+	var Person = {
+
+	name: form.username.value;
+	
+};
+
+	var html = "<p>You typed:" + Person.name + ".  </p>";
+
+	document.getElementById("displayText").innerHTML = html;
+
+};
+
+
+
+
