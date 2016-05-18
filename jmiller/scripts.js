c693626e36c52d104870654cac850fd445749cfc
Thebossmiller <jmiller34122@gmail.com>
2016-05-18 13:16:31 -0700

    Everything I have

---
+++ jmiller/scripts.js
@@ -0,0 +1,35 @@
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+  <link type = "text/css" rel = "stylesheet" href = "css/style.css">
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
