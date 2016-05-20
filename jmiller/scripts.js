a91912910b41c8f1c7c97147bf9bc277bffda410
Thebossmiller <jmiller34122@gmail.com>
2016-05-20 13:32:33 -0700

    madlibs

--- jmiller/scripts.js
+++ jmiller/scripts.js
@@ -1,17 +1,30 @@
+var form = document.forms.practiceForm;
 
+form.addEventListener("submit", displayPerson, false); 
 
-<link href="css/bootstrap.min.css" rel="stylesheet">
-  <link type = "text/css" rel = "stylesheet" href = "css/style.css">
+function displayPerson (event) {
+  event.preventDefault();
 
+  var Person = {
 
+  name: form.username.value,
+  age: form.userage.value,
+  name: form.username.value,
+  sex: form.username.value,
+};
 
+  var words = "<p> The last survivor" + Person.name + "lost at the age of" + Person.age + "tooken captive by"
+  + Person.name + "the last thing that he/she remembers"                 "</p>";
+
+  document.getElementById("displayText").innerHTML = words;
+
+};
 
   
 
 
 
-
-
+  
 
 
 
