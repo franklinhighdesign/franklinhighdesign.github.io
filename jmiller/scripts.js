772613ca65321fa45e8349a6ffe4d38a2d6a283e
Thebossmiller <jmiller34122@gmail.com>
2016-05-26 13:42:35 -0700

    update of madlibs completed

--- jmiller/scripts.js
+++ jmiller/scripts.js
@@ -9,19 +9,19 @@ function displayPerson (event) {
 
   name: form.username.value,
   age: form.userage.value,
-  name: form.username.value,
-  sex: form.username.value,
+  name2: form.username2.value,
+  sex: form.usersex.value,
+  memory: form.usermemory.value,
+  nightmare: form.usernightmare.value,
 };
 
-  var words = "<p> The last survivor" + Person.name + "lost at the age of" + Person.age + "tooken captive by"
-  + Person.name + "the last thing that he/she remembers"                 "</p>";
+  var words = "<p> The only remaining member of his family  " + Person.name + " lost at the age of " + Person.age + " tooken captive by " + Person.name2 + " the last thing that he/she remembers " + Person.sex + + Person.memory  + " is their worst nightmare " + Person.nightmare +" they enter into the pit of great sorrow you weep  ,  tears roll down their face then they scream it was just a dream ." + "</p>"; 
 
   document.getElementById("displayText").innerHTML = words;
 
 };
 
   
-
 
 
   
