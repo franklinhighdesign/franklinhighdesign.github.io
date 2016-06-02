8656999e18533c6379c6ad17eebd793a928b58d3
ecarr1602 <ethancarr1707@gmail.com>
2016-06-02 13:13:47 -0700

    fixing stuff

--- ecarr/RPS final/js/scripts.js
+++ ecarr/RPS final/js/scripts.js
@@ -53,9 +53,59 @@ function computerPicker (){
 
 
 function compare () {
+	if (userChoice === comChoice) {
+		var html = "<p>" + "<h2>" + "The winner is:" + " No one! It was a tie! " + "</h2>" + "</p>"
+		document.getElementById("displayText").innerHTML = html;
+		console.log("The result is a tie!");
+		//Tie
+	}
 
 
+	else if(userChoice === "scissors" && comChoice === "paper") {
+		var html = "<p>" + "<h2>" + "The winner is:" + " You're winner! By choosing scissors and cutting its paper!" + "</h2>" + "</p>"
+		document.getElementById("displayText").innerHTML = html;
+		console.log("The Computer chose rock, and won!");
+		//Player wins
+	}
 
 
+	else if(userChoice === "rock" && comChoice === "scissors") {
+		var html = "<p>" + "<h2>" + "The winner is:" + " You're winner! By choosing rock and crushing its scissors!" + "</h2>" + "</p>"
+		document.getElementById("displayText").innerHTML = html;
+		console.log("Computer chose scissors, and lost!");
+		//Player wins
+	}
 
-}
+
+	else if(userChoice === "paper" && comChoice === "rock") {
+		var html = "<p>" + "<h2>" + "The winner is:" + " You're winner! By choosing paper and covering its rock!" + "</h2>" + "</p>"	
+		document.getElementById("displayText").innerHTML = html;
+		console.log("The Computer chose rock, and lost!");
+		//Player wins
+	}
+
+
+	else if(userChoice === "paper" && comChoice === "scissors") {
+		var html = "<p>" + "<h2>" + "The winner is:" + " The computer! By choosing scissors and cutting your paper!" + "</h2>" + "</p>"
+		document.getElementById("displayText").innerHTML = html;
+		console.log("The Computer chose scissors, and won!");
+		//Computer wins
+	}
+
+
+	else if(userChoice === "scissors" && comChoice === "rock") {
+		var html = "<p>" + "<h2>" + "The winner is:" + " The computer! By choosing rock and crushing your scissors!" + "</h2>" + "</p>"
+		document.getElementById("displayText").innerHTML = html;
+		console.log("The Computer chose rock, and won!");
+		//Computer wins
+	}
+
+
+	else if(userChoice === "rock" && comChoice === "paper") {
+		var html = "<p>" + "<h2>" + "The winner is:" + " The computer! By choosing paper and covering your rock!" + "</h2>" + "</p>"
+		document.getElementById("displayText").innerHTML = html;
+		console.log("Computer chose paper, and beats rock!");
+		//Computer wins
+	}
+
+};
